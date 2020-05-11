869803ea0373e80f0f361cc61bd83d3792bf14c6
nenadlazic8 <45943346+nenadlazic8@users.noreply.github.com>
2020-05-11 11:55:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+ function darkMode() {
+    var ball = document.getElementById("ball");
+    var toggleItem = document.getElementById("toggle");
+    var body = document.getElementById("body");
+    var box = document.getElementsByClassName("box");
+    ball.classList.toggle('left');
+
+    if(ball.classList.contains('left')){
+        ball.style.left = '50%';
+        toggleItem.style.background = 'linear-gradient(hsl(146, 68%, 55%), hsl(210, 78%, 86%))';
+        body.style.backgroundColor = 'hsl(0, 0%, 100%)';
+        body.style.color = 'black';
+
+        for(let i = 0; i < box.length; i++){
+            box[i].style.backgroundColor = 'hsl(227, 47%, 96%)';
+        }
+        
+    }else{
+        ball.style.left = '5%';
+        toggleItem.style.background = 'linear-gradient(hsl(210, 78%, 56%), hsl(146, 68%, 55%))';
+        body.style.backgroundColor = 'hsl(230, 17%, 14%)';
+        body.style.color = 'white';
+
+        for(let i = 0; i < box.length; i++){
+            box[i].style.backgroundColor = 'hsl(228, 28%, 20%)';
+        }
+    }
+  }
